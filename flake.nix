dbc87ba7aaec558764cfadffd5119b88ef69907a
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2024-02-23 23:05:33 +0000

    oh, that's what that does

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     # this should work. see https://discourse.nixos.org/t/flakes-re-locking-necessary-at-each-evaluation-when-import-sub-flake-by-path/34465/6
     #localConfig.url = "path:.config";
     localConfig.url = "git+file:.?dir=./.config";
+    localConfig.flake = false;
     drduhConfig.flake = false;
   };
 
